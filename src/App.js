fac8255a086c43d1c3aedb3cfead2459a28969d3
muddassirk <mkkhanu72@gmail.com>
2021-04-30 00:24:14 +0500

    initialize react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NavBar from './components/NavBar';
+import InfoPanel from './components/InfoPanel';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <InfoPanel />
+    </div>
+  );
+}
+
+export default App;
