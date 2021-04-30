308bfdea2bc268f28f2d877f17db48f0326a9c67
muddassirk <mkkhanu72@gmail.com>
2021-04-30 15:12:32 +0500

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import NavBar from './components/NavBar';
 import InfoPanel from './components/InfoPanel';
 import './App.css';
+import FootNav from './components/FootNav';
+import { useState } from 'react';
 
 function App() {
+
+  const screenConfig = useState(0)
+
   return (
     <div>
       <NavBar />
       <InfoPanel />
+      <FootNav screenConfig={screenConfig} />
     </div>
   );
 }
