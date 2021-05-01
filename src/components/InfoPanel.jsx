bdc29845c34f422409c9045937bd6bb5ae6959bf
muddassirk <mkkhanu72@gmail.com>
2021-05-01 15:07:04 +0500

    changes in infoPanel

--- src/components/InfoPanel.jsx
+++ src/components/InfoPanel.jsx
@@ -1,71 +1,12 @@
 import React, { useEffect, useState } from 'react';
-
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 1000,
-    margin: '0 auto',
-    marginTop: 50
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-  total :{
-    color:'#3f51b5'
-  }
-}));
-
-export default function InfoPanel() {
-
-  const [globalCovidData, setglobalCovidData] = useState({})
+import GlobalStats from './GlobalStats';
+import AllCountries from './AllCountries';
 
 
-  useEffect(() => {
-    async function getData() {
-      const response = await fetch('https://api.covid19api.com/summary')
+export default function InfoPanel({ currentScreen }) {
 
-      let data = await response.json()
-      // console.log(data);
-      // console.log(data.Global);
-      setglobalCovidData(data.Global)
-      console.log(globalCovidData);
-    }
-    getData()
-
-  }, [])
-
-
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-
-        {Object.keys(globalCovidData).map((key, index) => {
-          return (
-
-            <Grid key={index} item xs={12} sm={4}>
-              <Paper
-                className={classes.paper}
-                elevation={3}>
-                 
-                <h3 className={classes.total}>{key}</h3>
-                {/* <h3>{key.replace(/_/g,' ').toUpperCase()}</h3> */}
-                <h3>{globalCovidData[key]}</h3>
-              </Paper>
-            </Grid>
-          )
-          //TIming 48:45
-        })}
-
-
-
-      </Grid>
-    </div>
-  );
+  if (currentScreen === 0)
+    return <GlobalStats />
+  else if (currentScreen === 1)
+    return <AllCountries />
 }
